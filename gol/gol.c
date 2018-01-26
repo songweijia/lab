dfd5fe0bcfb99c24265514f56c12a697f49eb562
Weijia Song <ws393@en-ec-ath-08.coecis.cornell.edu>
2018-01-26 02:37:30 -0500

    added multiple thread support.

--- gol/gol.c
+++ gol/gol.c
@@ -3,6 +3,9 @@
 #include <errno.h>
 #include <time.h>
 #include <string.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <errno.h>
 /*
   Shape of a 8x8 World: 
   The actual space in the world is 6x6
@@ -24,11 +27,11 @@
   (w[((y)<<(ox))+(x)])
 
 #ifdef DEBUG
-void dumpWorld(int *world, int upper_slot, int ncol_with_margin, int nrow_with_margin) {
+void dumpWorld(int *world, int lower_slot, int ncol_with_margin, int nrow_with_margin) {
   int i,j;
 
   printf("START>\n");
-  int ofst = (upper_slot)?(ncol_with_margin*nrow_with_margin):0;
+  int ofst = (lower_slot)?0:(ncol_with_margin*nrow_with_margin);
   for(i=0;i<nrow_with_margin;i++) {
     for(j=0;j<ncol_with_margin;j++) {
       if(world[i*ncol_with_margin+ofst+j])
@@ -42,23 +45,22 @@ void dumpWorld(int *world, int upper_slot, int ncol_with_margin, int nrow_with_m
 }
 #endif
 
-int step(int *world,int order_of_ncol,int nrow,int upper_slot) {
+int step(int *w1,int *w2, int order_of_ncol,int nrow) {
 #ifdef DEBUG
-  int * _world_from  = world + (upper_slot<<order_of_ncol)*(nrow+2);
-  int * _world_to  = world + ((1-upper_slot)<<order_of_ncol)*(nrow+2);
+  int * _world_from = w1;
+  int * _world_to = w2;
   int _oncol  = order_of_ncol;
   int _ncol  = (1<<_oncol) - 2;
   int _nrow  = nrow;
   for (int y = 1; y <= _nrow; y++) {
 #else
-  register int * _world_from asm("r15") = world + (upper_slot<<order_of_ncol)*(nrow+2);
-  register int * _world_to asm("r14") = world + ((1-upper_slot)<<order_of_ncol)*(nrow+2);
+  register int * _world_from asm("r15") = w1;
+  register int * _world_to asm("r14") = w2;
   register int _oncol asm("r13") = order_of_ncol;
   register int _ncol asm("r12") = (1<<_oncol) - 2;
   register int _nrow asm("r10") = nrow;
   for (register int y asm("r9") = 1; y <= _nrow; y++) {
 #endif
-
 
     // initialize l/c/r cols.
     //
@@ -170,34 +172,145 @@ int destroy(int **world){
   return 0;
 }
 
+//Thread parameters
+typedef struct {
+  int *w1, *w2;
+  int order_of_ncol;
+  int nrow;
+  int num_of_generations;
+  sem_t s1; 
+  sem_t *ps2;
+} ThreadParams;
+
+//Thread
+void * thread_routine(void * param) {
+  ThreadParams* tp = (ThreadParams*)param;
+  while(tp->num_of_generations -- ){
+    int direction = (tp->num_of_generations%2);
+    // 1 - waiting on semaphore
+#ifdef DEBUG
+      printf("Thread waiting for sem@%p\n",&tp->s1);
+#endif
+    if(sem_wait(&tp->s1)){
+      printf("error wait for sempahore.\n");
+      return NULL;
+    }
+    // 2 - do the work:
+    step(direction?tp->w1:tp->w2,direction?tp->w2:tp->w1,tp->order_of_ncol,tp->nrow);
+    // 3 - post the direction
+#ifdef DEBUG
+      printf("Thread posting to sem@%p\n",tp->ps2);
+#endif
+    if(sem_post(tp->ps2)){
+      printf("error post to semaphore.\n");
+      return NULL;
+    }
+  };
+  return NULL;
+}
+
+//semaphores.
+sem_t sem2;
+
 int main(int argc, char **argv) {
-  if(argc < 3) {
-    printf("USAGE: %s <order of number of cols> <number of rows>  [Generation]\n",argv[0]);
+  if(argc < 4) {
+    printf("USAGE: %s <order of number of cols> <number of rows> <number of thread> [Generation]\n",argv[0]);
     return -1;
   }
 
   int order_of_ncol = atoi(argv[1]);
   int nrow = atoi(argv[2]);
+  int num_of_threads = atoi(argv[3]);
+  int nrow_thread = (nrow+num_of_threads-1)/num_of_threads;
+  int nrow_last_thread = (nrow%nrow_thread)?(nrow%nrow_thread):nrow_thread;
   int num_of_generations;
-  if (argc >=4)
-    num_of_generations = atoi(argv[3]);
+  if (argc >=5)
+    num_of_generations = atoi(argv[4]);
   else
-    num_of_generations = (1L<<30) / ((1L<<order_of_ncol)*(nrow+2)); // do ~1GB cells by default.
+    num_of_generations = (1L<<30) / ((1L<<order_of_ncol)*(nrow+2)) * num_of_threads; // do ~1GB cells by default.
   int gcount = num_of_generations;
   int *world = NULL;
 
+  if(sem_init(&sem2,0,0)) {
+    printf("failed to initialize semaphore,errno=%d\n",errno);
+    return -1;
+  }
+
+  // initialize world
   if(initialize(&world,order_of_ncol,nrow)!=0) {
     return -1;
   }
 
+  // start threads.
+  ThreadParams *tps = (ThreadParams*)malloc(sizeof(ThreadParams)*num_of_threads);
+  if(tps==NULL){
+    fprintf(stderr,"Fail to allocate memory for ThreadParameters.\n");
+    return -1;
+  }
+  pthread_t *ths = (pthread_t*)malloc(sizeof(pthread_t)*num_of_threads);
+  if(ths==NULL){
+    fprintf(stderr,"Fail to allocate memory for thread identifiers.\n");
+    return -1;
+  }
+
+  for(int i=0;i<num_of_threads;i++){
+    tps[i].w1 = world + (1<<order_of_ncol)*(nrow_thread)*i;
+    tps[i].w2 = tps[i].w1 + (1<<order_of_ncol)*(nrow+2);
+    tps[i].order_of_ncol = order_of_ncol;
+    tps[i].nrow = nrow_thread;
+    tps[i].num_of_generations = num_of_generations;
+    if(sem_init(&tps[i].s1,0,0)){
+      printf("failed to initialize semaphore,errno=%d\n",errno);
+      return -1;
+    }
+    tps[i].ps2 = &sem2;
+#ifdef DEBUG
+    printf("tps[%d].w1=%p.\n",i,tps[i].w1);
+    printf("tps[%d].w2=%p.\n",i,tps[i].w2);
+    printf("tps[%d].order_of_ncol=%d.\n",i,tps[i].order_of_ncol);
+    printf("tps[%d].nrow=%d.\n",i,tps[i].nrow);
+    printf("tps[%d].num_of_generations=%d.\n",i,tps[i].num_of_generations);
+#endif
+  }
+  tps[num_of_threads-1].nrow = nrow_last_thread;
+
+  for(int i=0;i<num_of_threads;i++) {
+    if(pthread_create(&ths[i],NULL,thread_routine,&tps[i])) {
+      fprintf(stderr,"Failed to create thread, errno=%d.\n",errno);
+      return -1;
+    }
+  }
+  
   struct timespec ts,te;
   clock_gettime(CLOCK_REALTIME,&ts);
-  while(gcount--) {
+
+  while(gcount -- ) {
 #ifdef DEBUG
     dumpWorld(world, gcount%2, 1<<order_of_ncol,nrow+2);
 #endif
-    step(world,order_of_ncol,nrow,gcount%2);
+    // Start threads.
+    int i;
+    for(i=0;i<num_of_threads;i++){
+#ifdef DEBUG
+      printf("posting to sem@%p\n",&tps[i].s1);
+#endif
+      if(sem_post(&tps[i].s1)) {
+        fprintf(stderr,"Failed to post semaphore.errno=%d\n",errno);
+        return -1;
+      }
+    }
+    // wait for results.
+    for(i=0;i<num_of_threads;i++){
+#ifdef DEBUG
+      printf("waiting for sem@%p\n",&sem2);
+#endif
+      if(sem_wait(&sem2)) {
+        fprintf(stderr,"Failed to wait semaphore.errno=%d\n",errno);
+        return -1;
+      }
+    }
   }
+
   clock_gettime(CLOCK_REALTIME,&te);
 
   ssize_t tot_ns = (te.tv_sec-ts.tv_sec)*1000000000 + (te.tv_nsec-ts.tv_nsec);
@@ -206,5 +319,11 @@ int main(int argc, char **argv) {
   printf("%.3f second\n",(double)tot_ns/1000000000);
   printf("%ld KB working set;\t throughput = %.3fM cell generation per sec.\n", nworld>>7, (double)ncell*num_of_generations/tot_ns*1000);
 
+  for(int i=0;i<num_of_threads;i++){
+    sem_destroy(&tps[i].s1);
+  }
+  free(tps);
+  free(ths);
+  sem_destroy(&sem2);
   destroy(&world);
 }
--- gol/run.sh
+++ gol/run.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-for((l=1;l<=1;l++))
+for((l=1;l<=5;l++))
 do
-  for((i=10;i<=1024;i+=4))
+  for((i=10;i<=16384;i+=16))
   do
   #  sudo perf stat -e instructions,cycles,cpu/event=0xa2,umask=0x10,cmask=0/ taskset 0x80 ./gol 10 $i 1000 2>> output
-    sudo rdtset -t 'l3=0xfffff;cpu=4' -k -c 4 nice --20 ./gol 10 $i >> output.$l
+    for nth in 1 2 4 8 16
+    do
+      for way in 1 3 f ff ffff
+      do
+        echo "sudo rdtset -t \"l3=0x${way};cpu=1-${nth}\" -k -c ${nth} nice --20 ./gol 10 $i ${nth} >> data/${nth}-th-${way}-way-output.$l"
+        sudo rdtset -t "l3=0x${way};cpu=1-${nth}" -k -c 1-${nth} nice --20 ./gol 10 $i ${nth} >> data/${nth}-th-${way}-way-output.$l
+      done
+    done
   done
 done
